4ad20849850da5cfde6a490745b2348da22a850e
ThanasisTs <thtsitos@gmail.com>
2020-03-30 10:27:46 +0300

    merged launch files

--- use_case_traj_exec/scripts/trajectory_csv_example.py
+++ use_case_traj_exec/scripts/trajectory_csv_example.py
@@ -6,7 +6,7 @@ from trajectory_execution_msgs.msg import PointArray
 from geometry_msgs.msg import Point
 
 def main():
-	rospy.init_node('csv_to_motion')
+	rospy.init_node('trajectory_csv_example')
 	pub = rospy.Publisher('trajectory_points', PointArray, queue_size=10)
 	file = open(sys.argv[1], 'r')
 	rd = csv.reader(file)
@@ -17,7 +17,7 @@ def main():
 		point.y = float(row[1])
 		point.z = float(row[2])
 		waypoints.points.append(point)
-	rospy.sleep(0.2)
+	rospy.sleep(2)
 	pub.publish(waypoints)
 	print ("Published the waypoints")
 
